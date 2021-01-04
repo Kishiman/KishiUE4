83fbbff57e1f55bc29f012e44231678af1fde007
kishiman <fares.manaisademx@gmail.com>
2021-01-04 22:54:28 +0100

    removed Kishi

--- Source/KishiLib/Kishi/include/Expression/Base/ExpressionInc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "KishiLibDependencies.h"
-#include "Expression/ThOperator.h"
-#include "Expression/ThOperatorInt.h"
-#include "Expression/ThOperatorLV.h"
-
-
-
--- Source/KishiLib/Kishi/include/Theory/Base/TheoryInc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "KishiLibDependencies.h"
-#include "Theory/ThClone.h"
-#include "Theory/ThEqual.h"
-
-
-
